ba80a15c3c0efdce7adec3bc7c441bdbcf66a5d9
Matheus N Leite <matheusn06leite@gmail.com>
2024-03-05 12:12:41 -0300

    Inicio da pagina 5 do beecrowd

---
+++ C/acimaDaDiagonalPrincipal.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    float M[12][12],media=0,soma=0;
+    char t;
+    int cont=0;
+
+
+    scanf(" %c",&t);
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%f",&M[i][j]);
+            if (j>i)
+            {
+                soma = soma + M[i][j];
+                cont++;
+            }
+            
+        }
+        
+    }
+
+        
+        
+        
+    if (t=='M')
+    {
+        media = soma/cont;
+        printf("%.1f\n",media);
+        
+    }else if (t=='S')
+    {
+        printf("%.1f\n",soma);
+    }
+    
+    
+    
+    
+}
---
+++ C/colunaNaMatriz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    float M[12][12],media=0,soma=0;
+    char t;
+    int c;
+
+    scanf("%d",&c);
+    scanf(" %c",&t);
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%f",&M[i][j]);
+        }
+        
+    }
+
+        for (int i = 0; i < 12; i++)
+        {
+            soma = soma + M[i][c];
+        }
+        
+        
+    if (t=='M')
+    {
+        media = soma/12;
+        printf("%.1f\n",media);
+        
+    }else if (t=='S')
+    {
+        printf("%.1f\n",soma);
+    }
+    
+    
+    
+    
+}
