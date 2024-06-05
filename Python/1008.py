3a834ab8e77055ed52e8c4acbb9d7d4cafe0a95a
Matheus <matheusn06leite@gmail.com>
2024-06-05 11:27:19 -0300

    inicio dos exercicios do beecrowd em python ate o 1009

---
+++ Python/1008.py
@@ -0,0 +1,8 @@
+n=int(input())
+h=int(input())
+salary=float(input())
+
+salary = salary * h
+
+print("NUMBER = %d" %n)
+print("SALARY = U$ {:.2f}".format(salary))
---
+++ Python/1009.py
@@ -0,0 +1,7 @@
+name = input()
+salary = float(input())
+totv = float(input())
+
+salary = salary + (totv * 0.15)
+
+print("TOTAL = R$ {:.2f}".format(salary))
